f29ede05dc3d2fd2716c1d0d0f1ed6e712f17e4f
Aidin <bekturganovaidin@gmail.com>
2020-08-06 14:03:19 +0600

    Initial Commit

---
+++ TodoList2.0/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TodoList2.0
+//
+//  Created by User on 8/6/20.
+//  Copyright © 2020 Aidin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
